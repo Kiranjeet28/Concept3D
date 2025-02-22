526a28113f643aed70b24eb19d1c52e081d79de6
kiranjeet28 <kiranjeetkour144@gmail.com>
2025-02-22 22:40:19 +0700

    About section improve

--- ReactApp/src/sections/Aboutus.jsx
+++ ReactApp/src/sections/Aboutus.jsx
@@ -20,10 +20,10 @@ const About = () => {
             <div className="grid xl:grid-cols-3 xl:grid-rows-6 md:grid-cols-2 grid-cols-1 gap-5 h-full">
                 <div className="col-span-1 xl:row-span-3">
                     <div className="grid-container">
-                        <img src="assets/grid1.png" alt="grid-1" className="w-full sm:h-[276px] h-fit object-contain" />
+                        <img src="assets/about1.jpg" alt="grid-1" className="w-full sm:h-[276px] h-fit object-contain rounded-md" />
 
                         <div>
-                            <p className="grid-headtext">Hi, I’m Adrian Hajdin</p>
+                            <p className="grid-headtext">Hi, I’m Kiranjeet Kour</p>
                             <p className="grid-subtext">
                                 With 12 years of experience, I have honed my skills in both frontend and backend dev, creating dynamic
                                 and responsive websites.
@@ -58,12 +58,12 @@ const About = () => {
                                 showGraticules
                                 globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
                                 bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
-                                labelsData={[{ lat: 40, lng: -100, text: 'Rjieka, Croatia', color: 'white', size: 15 }]}
+                                labelsData={[{ lat: 40, lng: -100, text: 'Ludhiana, India', color: 'white', size: 15 }]}
                             />
                         </div>
                         <div>
                             <p className="grid-headtext">I’m very flexible with time zone communications & locations</p>
-                            <p className="grid-subtext">I&apos;m based in Rjieka, Croatia and open to remote work worldwide.</p>
+                            <p className="grid-subtext">I&apos;m based in Ludhiana, Croatia and open to remote work worldwide.</p>
                             <Button name="Contact Me" isBeam containerClass="w-full mt-10" />
                         </div>
                     </div>
