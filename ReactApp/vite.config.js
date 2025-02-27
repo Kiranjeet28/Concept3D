befc50b5c2586fa4f143f14906d9336c2e6fca0c
kiranjeet28 <kiranjeetkour144@gmail.com>
2025-02-27 13:36:52 +0530

    Build error solve

--- ReactApp/vite.config.js
+++ ReactApp/vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    target: 'esnext', // Allow modern JS features like top-level await
+  },
+  esbuild: {
+    target: 'esnext', // Ensure esbuild uses modern syntax
+  },
 })
