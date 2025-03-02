0c2997ebf64983f948ca9a0e644001778ef5c63c
kiranjeet28 <kiranjeetkour144@gmail.com>
2025-03-02 18:00:01 +0530

    tabular array

--- ReactApp/vite.config.js
+++ ReactApp/vite.config.js
@@ -16,3 +16,17 @@ export default defineConfig({
     include: ['@tabler/icons-react'], // Ensure it's pre-bundled
   },
 })
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     target: "esnext", // Ensure latest JS features
+//   },
+//   optimizeDeps: {
+//     esbuildOptions: {
+//       target: "esnext", // Fix top-level await issue
+//     },
+//   },
+// });
