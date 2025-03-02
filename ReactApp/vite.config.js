69925d8defc86c0b55b03de1f88aeaf8fb4b0bd9
kiranjeet28 <kiranjeetkour144@gmail.com>
2025-03-02 17:51:01 +0530

    add

--- ReactApp/vite.config.js
+++ ReactApp/vite.config.js
@@ -1,13 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     target: 'esnext', // Allow modern JS features like top-level await
+    rollupOptions: {
+      external: ['@tabler/icons-react'], // Exclude the package from bundling
+    },
   },
   esbuild: {
     target: 'esnext', // Ensure esbuild uses modern syntax
+  },
+  optimizeDeps: {
+    include: ['@tabler/icons-react'], // Ensure it's pre-bundled
   },
 })
